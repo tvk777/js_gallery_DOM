0f8459965d1b546d818c044d94aed54e567fd1d0
tvk777 <klimenko.tv7@gmail.com>
2025-08-13 17:47:35 +0300

    make build

---
+++ index.ad57abbe.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var n=t.target.closest("a.list-item__link");n&&(t.preventDefault(),e.src=n.href)});
+//# sourceMappingURL=index.ad57abbe.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const bigImg = document.getElementById("largeImg");
+const thumbs = document.getElementById("thumbs");
+const thumbHandler = (e)=>{
+    const link = e.target.closest("a.list-item__link");
+    if (!link) return;
+    e.preventDefault();
+    bigImg.src = link.href;
+};
+thumbs.addEventListener("click", thumbHandler);
+
+//# sourceMappingURL=index.f75de5e1.js.map
